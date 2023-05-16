6a597117d8c6b775322091b54d3ef41449aa8656
Ilham Irfandi <inginkandia1995@gmail.com>
2023-05-16 09:41:46 +0700

    first commit

---
+++ inventory.sql
@@ -0,0 +1,208 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Waktu pembuatan: 02 Bulan Mei 2023 pada 06.20
+-- Versi server: 10.4.27-MariaDB
+-- Versi PHP: 8.0.25
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `inventory`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `notes`
+--
+
+CREATE TABLE `notes` (
+  `id` int(11) NOT NULL,
+  `contents` text NOT NULL,
+  `admin` varchar(20) NOT NULL,
+  `status` varchar(8) NOT NULL DEFAULT 'aktif'
+) ENGINE=MyISAM DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+--
+-- Dumping data untuk tabel `notes`
+--
+
+INSERT INTO `notes` (`id`, `contents`, `admin`, `status`) VALUES
+(24, 'hallo selamat datang', 'Admin', 'aktif');
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `sbrg_keluar`
+--
+
+CREATE TABLE `sbrg_keluar` (
+  `id` int(11) NOT NULL,
+  `idx` int(11) NOT NULL,
+  `tgl` date NOT NULL,
+  `jumlah` int(11) NOT NULL,
+  `penerima` varchar(35) NOT NULL,
+  `keterangan` text NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+--
+-- Dumping data untuk tabel `sbrg_keluar`
+--
+
+INSERT INTO `sbrg_keluar` (`id`, `idx`, `tgl`, `jumlah`, `penerima`, `keterangan`) VALUES
+(17, 246, '2023-04-27', 5, 'Junaidy', 'Milik kantor'),
+(18, 246, '2023-03-08', 2, 'Erin', 'Mohon diJjga dengan baik');
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `sbrg_masuk`
+--
+
+CREATE TABLE `sbrg_masuk` (
+  `id` int(11) NOT NULL,
+  `idx` int(11) NOT NULL,
+  `tgl` date NOT NULL,
+  `jumlah` int(11) NOT NULL,
+  `keterangan` text NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+--
+-- Dumping data untuk tabel `sbrg_masuk`
+--
+
+INSERT INTO `sbrg_masuk` (`id`, `idx`, `tgl`, `jumlah`, `keterangan`) VALUES
+(13, 248, '2023-04-27', 5, 'Ilham Irfandi'),
+(14, 246, '2023-05-02', 15, 'Ilham Irfandi');
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `slogin`
+--
+
+CREATE TABLE `slogin` (
+  `id` int(11) NOT NULL,
+  `username` varchar(30) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `nickname` varchar(20) NOT NULL,
+  `role` varchar(10) NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+--
+-- Dumping data untuk tabel `slogin`
+--
+
+INSERT INTO `slogin` (`id`, `username`, `password`, `nickname`, `role`) VALUES
+(7, 'guest', '084e0343a0486ff05530df6c705c8bb4', 'Admin', 'stock');
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `sstock_brg`
+--
+
+CREATE TABLE `sstock_brg` (
+  `idx` int(11) NOT NULL,
+  `nama` varchar(55) NOT NULL,
+  `jenis` varchar(30) NOT NULL,
+  `merk` varchar(40) NOT NULL,
+  `ukuran` varchar(20) NOT NULL,
+  `stock` int(12) NOT NULL,
+  `satuan` varchar(10) NOT NULL,
+  `lokasi` varchar(55) NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+--
+-- Dumping data untuk tabel `sstock_brg`
+--
+
+INSERT INTO `sstock_brg` (`idx`, `nama`, `jenis`, `merk`, `ukuran`, `stock`, `satuan`, `lokasi`) VALUES
+(246, 'Laptop', 'komputer', 'ASUS ROG', '44 cm', 23, 'pack', 'Gudang nomor 01'),
+(247, 'Meja', 'Kayu Jati', 'IKEA', '50 cm', 10, 'Unit', 'Gudang nomor 01'),
+(245, 'Kursi', 'Kayu Jati', 'IKEA', '44 cm', 15, 'Unit', 'Gudang nomor 01'),
+(248, 'PC', 'komputer', 'ASUS ROG', '44 cm', 5, 'Buah', 'Gudang nomor 01');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indeks untuk tabel `notes`
+--
+ALTER TABLE `notes`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indeks untuk tabel `sbrg_keluar`
+--
+ALTER TABLE `sbrg_keluar`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indeks untuk tabel `sbrg_masuk`
+--
+ALTER TABLE `sbrg_masuk`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indeks untuk tabel `slogin`
+--
+ALTER TABLE `slogin`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indeks untuk tabel `sstock_brg`
+--
+ALTER TABLE `sstock_brg`
+  ADD PRIMARY KEY (`idx`);
+
+--
+-- AUTO_INCREMENT untuk tabel yang dibuang
+--
+
+--
+-- AUTO_INCREMENT untuk tabel `notes`
+--
+ALTER TABLE `notes`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=25;
+
+--
+-- AUTO_INCREMENT untuk tabel `sbrg_keluar`
+--
+ALTER TABLE `sbrg_keluar`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=19;
+
+--
+-- AUTO_INCREMENT untuk tabel `sbrg_masuk`
+--
+ALTER TABLE `sbrg_masuk`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
+
+--
+-- AUTO_INCREMENT untuk tabel `slogin`
+--
+ALTER TABLE `slogin`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
+
+--
+-- AUTO_INCREMENT untuk tabel `sstock_brg`
+--
+ALTER TABLE `sstock_brg`
+  MODIFY `idx` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=249;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
